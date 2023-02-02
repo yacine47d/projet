1becd7c1d55ef4120ad1ebc0b784cde6420d45a2
yacine <yacbh42697@gmail.com>
2023-02-02 20:16:50 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,71 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
+import { Button, Tab } from 'react-bootstrap';
 import reactLogo from './assets/react.svg'
-import './App.css'
+import Nava from './nava';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+const video=[{
+  src: `https://www.youtube.com/embed/ufPCITfUQnQ?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `CSS-in-JS — Max Stoiber`,
+},
+{
+  src: `https://www.youtube.com/embed/v1JAUiqskiw?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Modern Forms in React — Erik Rasmussen`,
+},
+{
+  src: `https://www.youtube.com/embed/N0wHweOu-LQ?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Engineering led design — Lauren Argenta`,
+},
+{
+  src: `https://www.youtube.com/embed/xxbc3wAztl0?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Rethinking Design Practices — Mark Dalgleish`,
+},
+{
+  src: `https://www.youtube.com/embed/xxbc3wAztl0?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Rethinking Design Practices — Mark Dalgleish`,
+},
+{
+  src: `https://www.youtube.com/embed/xxbc3wAztl0?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Rethinking Design Practices — Mark Dalgleish`,
+},
+{
+  src: `https://www.youtube.com/embed/xxbc3wAztl0?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Rethinking Design Practices — Mark Dalgleish`,
+},
+{
+  src: `https://www.youtube.com/embed/xxbc3wAztl0?list=PLMY9nrA_sDPrVDt46wA15QuAIoAcfBI-N`,
+  title: `Rethinking Design Practices — Mark Dalgleish`,
+},
+];
   return (
-    <div className="App">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+    
+    <div className="container" style={{width:"1000%",height:"100vh",border:"2px solid black" }}>
+      <Nava/>
+      
+
+      
+      <div className='row'>
+
+    
+      {
+
+        video.map(e=>(
+          <div className='col-3' >
+            <iframe src={e.src}></iframe>
+        <h5>
+      {e.title}
+        </h5>
+
+          </div>
+        ))
+      }
+        </div>
     </div>
 
   )
---
+++ src/nava.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './youtube.png' 
+
+const Nava = () => {
+    return (
+        <div className="nav">
+       <img src={logo} alt="jr" style={{width:"10%",height:"10vh"}}/>
+       <input type="search" style={{width:"10",height:"10",marginLeft:"350px"}}/>
+       
+
+      
+
+      
+            
+            
+        </div>
+    );
+}
+
+export default Nava;
+
